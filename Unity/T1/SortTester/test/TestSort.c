b600c29733a8b009bc29f923253383567c31ce64
AndreBLisboa <andrebergmannl@gmail.com>
2024-04-19 20:31:33 -0300

    First implementation testing equal arrays

--- Unity/T1/SortTester/test/TestSort.c
+++ Unity/T1/SortTester/test/TestSort.c
@@ -12,31 +12,39 @@ TEST_TEAR_DOWN(Sort)
 {
 }
 
-void ValidTesting(int *a, int length, int index){
-  const char* type = "On";
-  if (index<2){
+void ValidTesting(int *a, int length, int index)
+{
+  const char *type = "On";
+  if (index < 2)
+  {
     TEST_ASSERT_EQUAL(0, sort(a, length, (char *)type, index));
   }
-  else if (index >= 2 && index < 5){
+  else if (index >= 2 && index < 5)
+  {
     type = "On2";
     TEST_ASSERT_EQUAL(0, sort(a, length, (char *)type, index));
   }
-  else{
+  else
+  {
     type = "Onlogn";
     TEST_ASSERT_EQUAL(0, sort(a, length, (char *)type, index));
   }
 }
 
-void InvalidTesting(int *a, int length, int index){
-  const char* type = "On2";
-  if (index<2){
+void InvalidTesting(int *a, int length, int index)
+{
+  const char *type = "On2";
+  if (index < 2)
+  {
     TEST_ASSERT_EQUAL(1, sort(a, length, (char *)type, index));
   }
-  else if (index >= 2 && index < 5){
+  else if (index >= 2 && index < 5)
+  {
     type = "Onlogn";
     TEST_ASSERT_EQUAL(1, sort(a, length, (char *)type, index));
   }
-  else{
+  else
+  {
     type = "On";
     TEST_ASSERT_EQUAL(1, sort(a, length, (char *)type, index));
   }
@@ -44,92 +52,123 @@ void InvalidTesting(int *a, int length, int index){
 
 TEST(Sort, TestEverythingValid)
 {
-    int a[] = {1, 2, 3, 4, 5};
-    int length = 5;
+  int a[] = {1, 2, 3, 4, 5};
+  int length = 5;
 
-    for (int i=0; i<8; i++){
-      ValidTesting(a, length, i);
-    }
+  for (int i = 0; i < 8; i++)
+  {
+    ValidTesting(a, length, i);
+  }
 }
 
 TEST(Sort, TestInvalidType)
 {
-    int a[] = {1, 2, 3, 4, 5};
-    int length = 5;
-    const char* type = "T1";
+  int a[] = {1, 2, 3, 4, 5};
+  int length = 5;
+  const char *type = "T1";
 
-    for (int i=0; i<8; i++){
-      TEST_ASSERT_EQUAL(1, sort(a, length, (char *)type, i));
-    }
-
+  for (int i = 0; i < 8; i++)
+  {
+    TEST_ASSERT_EQUAL(1, sort(a, length, (char *)type, i));
+  }
 }
 
 TEST(Sort, TestOutOfOrderArray)
 {
-    int a[] = {2, 3, 1, 5, 4};
-    int length = 5;
+  int a[] = {5, 4, 3, 2, 1};
+  int arrayOrdered[] = {1, 2, 3, 4, 5};
+  int length = 5;
+  const char *type = "On";
+  char message[50];
 
-    for (int i=0; i<8; i++){
-      ValidTesting(a, length, i);
+  for (int i = 0; i < 8; i++)
+  {
+    sprintf(message, "Error in the index: %d", i);
+    for (int j = 5; j > 0; j--)
+    {
+      a[5 - j] = j;
     }
+    if (i < 2)
+    {
+      sort(a, length, (char *)type, i);
+    }
+    else if (i >= 2 && i < 5)
+    {
+      type = "On2";
+      sort(a, length, (char *)type, i);
+    }
+    else
+    {
+      type = "Onlogn";
+      sort(a, length, (char *)type, i);
+    }
+
+    TEST_ASSERT_EQUAL_INT32_ARRAY_MESSAGE(arrayOrdered, a, length, message);
+  }
 }
 
 TEST(Sort, TestWrongBiggerLenght)
 {
-    int a[] = {2, 3, 1, 5, 4};
-    int length = 5; // Valgrind error, used to be: int length = 6;
+  int a[] = {2, 3, 1, 5, 4};
+  int length = 5; // Valgrind error, used to be: int length = 6;
 
-    for (int i=0; i<8; i++){
-      ValidTesting(a, length, i);
-    }
+  for (int i = 0; i < 8; i++)
+  {
+    ValidTesting(a, length, i);
+  }
 }
 
 TEST(Sort, TestWrongSmallerLenght)
 {
-    int a[] = {2, 3, 1, 5, 4};
-    int length = 4;
+  int a[] = {2, 3, 1, 5, 4};
+  int length = 4;
 
-    for (int i=0; i<8; i++){
-      ValidTesting(a, length, i);
-    }
+  for (int i = 0; i < 8; i++)
+  {
+    ValidTesting(a, length, i);
+  }
 }
 
 TEST(Sort, TestTwoValuesArray)
 {
-    int a[] = {2, 1};
-    int length = 2;
+  int a[] = {2, 1};
+  int length = 2;
 
-    for (int i=0; i<8; i++){
-      ValidTesting(a, length, i);
-    }
+  for (int i = 0; i < 8; i++)
+  {
+    ValidTesting(a, length, i);
+  }
 }
 
 TEST(Sort, TestTwentyValuesArray)
 {
-    int a[] = {19, 17, 15, 13, 11, 9, 7, 5, 3, 1, 18, 16, 14, 12, 10, 8, 6, 4, 2, 0};
-    int length = 20;
+  int a[] = {19, 17, 15, 13, 11, 9, 7, 5, 3, 1, 18, 16, 14, 12, 10, 8, 6, 4, 2, 0};
+  int length = 20;
 
-    for (int i=0; i<8; i++){
-      ValidTesting(a, length, i);
-    }
+  for (int i = 0; i < 8; i++)
+  {
+    ValidTesting(a, length, i);
+  }
 }
 
 TEST(Sort, TestSameValuesArray)
 {
-    int a[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-    int length = 10;
+  int a[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+  int length = 10;
 
-    for (int i=0; i<8; i++){
-      ValidTesting(a, length, i);
-    }
+  for (int i = 0; i < 8; i++)
+  {
+    ValidTesting(a, length, i);
+  }
 }
 
 TEST(Sort, TestValidButWrongType)
 {
-    int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int length = 10;
+  int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int length = 10;
 
-    for (int i=0; i<8; i++){
-      InvalidTesting(a, length, i);
-    }
+  for (int i = 0; i < 8; i++)
+  {
+    InvalidTesting(a, length, i);
+  }
 }
