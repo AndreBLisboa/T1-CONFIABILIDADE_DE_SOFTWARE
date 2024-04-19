174a84f62b9403ba54cfeaef861b6d1ca34b854c
AndreBLisboa <andrebergmannl@gmail.com>
2024-04-19 13:42:16 -0300

    Added some tests

--- Unity/T1/SortTester/test/test_runners/TestSort_Runner.c
+++ Unity/T1/SortTester/test/test_runners/TestSort_Runner.c
@@ -5,4 +5,11 @@ TEST_GROUP_RUNNER(Sort)
 {
   RUN_TEST_CASE(Sort, TestEverythingValid);
   RUN_TEST_CASE(Sort, TestInvalidType);
+  RUN_TEST_CASE(Sort, TestOutOfOrderArray);
+  RUN_TEST_CASE(Sort, TestWrongBiggerLenght);
+  RUN_TEST_CASE(Sort, TestWrongSmallerLenght);
+  RUN_TEST_CASE(Sort, TestTwoValuesArray);
+  RUN_TEST_CASE(Sort, TestTwentyValuesArray);
+  RUN_TEST_CASE(Sort, TestSameValuesArray);
+  RUN_TEST_CASE(Sort, TestValidButWrongType);
 }
