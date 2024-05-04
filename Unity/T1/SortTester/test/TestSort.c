18162c5211ca2900bb870b7af97d4a5b5ad7a18c
AndreBLisboa <andrebergmannl@gmail.com>
2024-05-03 21:38:22 -0300

    New tests

--- Unity/T1/SortTester/test/TestSort.c
+++ Unity/T1/SortTester/test/TestSort.c
@@ -73,6 +73,98 @@ TEST(Sort, TestInvalidType)
   }
 }
 
+TEST(Sort, TestInvalidIntType)
+{
+  int a[] = {1, 2, 3, 4, 5};
+  int length = 5;
+  int type = 2;
+
+  TEST_ASSERT_EQUAL(1, sort(a, length, (char *)type, 8));
+
+}
+
+TEST(Sort, TestInvalidEmptyType)
+{
+  int a[] = {1, 2, 3, 4, 5};
+  int length = 5;
+  const char *type = "";
+
+  for (int i = 0; i < 8; i++)
+  {
+    TEST_ASSERT_EQUAL(1, sort(a, length, (char *)type, i));
+  }
+}
+
+TEST(Sort, TestValidButWrongType)
+{
+  int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int length = 10;
+
+  for (int i = 0; i < 8; i++)
+  {
+    InvalidTesting(a, length, i);
+  }
+}
+
+TEST(Sort, TestInvalidAlgorithm)
+{
+  int a[] = {1, 2, 3, 4, 5};
+  int length = 5;
+  const char *type = "On";
+
+  TEST_ASSERT_EQUAL(1, sort(a, length, (char *)type, 9));
+
+}
+
+TEST(Sort, TestInvalidCharAlgorithm)
+{
+  int a[] = {1, 2, 3, 4, 5};
+  int length = 5;
+  const char *type = "On";
+
+  TEST_ASSERT_EQUAL(1, sort(a, length, (char *)type, 'a'));
+
+}
+
+TEST(Sort, TestWrongBiggerLenght)
+{
+  int a[] = {2, 3, 1, 5, 4};
+  int length = 5; // Valgrind error, used to be: int length = 6;
+
+  for (int i = 0; i < 8; i++)
+  {
+    ValidTesting(a, length, i);
+  }
+}
+
+TEST(Sort, TestInvalidCharLenght)
+{
+  int a[] = {2, 3, 1, 5, 4};
+  char length = 'c';
+  const char *type = "On";
+  char message[50];
+
+  for (int i = 0; i < 8; i++)
+  {
+    sprintf(message, "Error in the index: %d", i);
+
+    if (i < 2)
+    {
+      TEST_ASSERT_EQUAL_MESSAGE(1, sort(a, length, (char *)type, i), message);
+    }
+    else if (i >= 2 && i < 5)
+    {
+      type = "On2";
+      TEST_ASSERT_EQUAL_MESSAGE(1, sort(a, length, (char *)type, i), message);
+    }
+    else
+    {
+      type = "Onlogn";
+      TEST_ASSERT_EQUAL_MESSAGE(1, sort(a, length, (char *)type, i), message);
+    }
+  }
+}
+
 TEST(Sort, TestOutOfOrderArray)
 {
   int a[] = {5, 4, 3, 2, 1};
@@ -106,17 +198,6 @@ TEST(Sort, TestOutOfOrderArray)
     {
       a[length - j] = j;
     }
-  }
-}
-
-TEST(Sort, TestWrongBiggerLenght)
-{
-  int a[] = {2, 3, 1, 5, 4};
-  int length = 5; // Valgrind error, used to be: int length = 6;
-
-  for (int i = 0; i < 8; i++)
-  {
-    ValidTesting(a, length, i);
   }
 }
 
@@ -205,47 +286,6 @@ TEST(Sort, TestSameValuesArray)
     TEST_ASSERT_EQUAL_INT32_ARRAY_MESSAGE(arrayOrdered, a, length, message);
   }
 
-}
-
-TEST(Sort, TestValidButWrongType)
-{
-  int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  int length = 10;
-
-  for (int i = 0; i < 8; i++)
-  {
-    InvalidTesting(a, length, i);
-  }
-}
-
-TEST(Sort, TestEmptyArray)
-{
-  int a[] = {};
-  int arrayOrdered[] = {};
-  int length = 0;
-  const char *type = "On";
-  char message[50];
-
-  for (int i = 0; i < 8; i++)
-  {
-    sprintf(message, "Error in the index: %d", i);
-    if (i < 2)
-    {
-      sort(a, length, (char *)type, i);
-    }
-    else if (i >= 2 && i < 5)
-    {
-      type = "On2";
-      sort(a, length, (char *)type, i);
-    }
-    else
-    {
-      type = "Onlogn";
-      sort(a, length, (char *)type, i);
-    }
-
-    TEST_ASSERT_EQUAL_INT32_ARRAY_MESSAGE(arrayOrdered, a, length, message);
-  }
 }
 
 TEST(Sort, TestOneValueArray)
@@ -338,7 +378,7 @@ TEST(Sort, TestLowerHalfArray)
 
     for (int k = 0; k < 10; k++)
     {
-      TEST_ASSERT_EQUAL_INT32_ARRAY_MESSAGE(arrayOrdered[k], a[k], length, message);
+      TEST_ASSERT_EQUAL_INT32_MESSAGE(arrayOrdered[k], a[k], message);
     }
 
     for (int j = length; j > 0; j--)
@@ -377,12 +417,142 @@ TEST(Sort, TestUpperHalfArray)
 
     for (int k = 10; k < 20; k++)
     {
-      TEST_ASSERT_EQUAL_INT32_ARRAY_MESSAGE(arrayOrdered[k], a[k], length, message);
+      TEST_ASSERT_EQUAL_INT32_MESSAGE(arrayOrdered[k], a[k], message);
     }
 
     for (int j = length; j > 0; j--)
     {
       a[length - j] = j;
+    }
+  }
+}
+
+TEST(Sort, TestEmptyArray)
+{
+  int a[] = {};
+  int arrayOrdered[] = {};
+  int length = 0;
+  const char *type = "On";
+  char message[50];
+
+  for (int i = 0; i < 8; i++)
+  {
+    sprintf(message, "Error in the index: %d", i);
+    if (i < 2)
+    {
+      sort(a, length, (char *)type, i);
+    }
+    else if (i >= 2 && i < 5)
+    {
+      type = "On2";
+      sort(a, length, (char *)type, i);
+    }
+    else
+    {
+      type = "Onlogn";
+      sort(a, length, (char *)type, i);
+    }
+
+    TEST_ASSERT_EQUAL_INT32_ARRAY_MESSAGE(arrayOrdered, a, length, message);
+  }
+}
+
+TEST(Sort, TestFloatValueArray)
+{
+  float a[] = {2.6, 3.7, 3.8, 3.9, 4, 1.1, 1.2, 1.3, 2.4, 2.5};
+  float arrayOrdered[] = {1.1, 1.2, 1.3, 2.4, 2.5, 2.6, 3.7, 3.8, 3.9, 4};
+  int length = 10;
+  const char *type = "On";
+  char message[50];
+
+  for (int i = 0; i < 8; i++)
+  {
+    sprintf(message, "Error in the index: %d", i);
+    
+    if (i < 2)
+    {
+      sort(a, length, (char *)type, i);
+    }
+    else if (i >= 2 && i < 5)
+    {
+      type = "On2";
+      sort(a, length, (char *)type, i);
+    }
+    else
+    {
+      type = "Onlogn";
+      sort(a, length, (char *)type, i);
+    }
+
+    TEST_ASSERT_EQUAL_FLOAT_ARRAY_MESSAGE(arrayOrdered, a, length, message);
+
+  }
+}
+
+TEST(Sort, TestCharValueArray)
+{
+  char a[] = {'a', 'b', 'c', 'd', 'e'};
+  char arrayOrdered[] = {'a', 'b', 'c', 'd', 'e'};
+  int length = 20;
+  const char *type = "On";
+  char message[50];
+
+  for (int i = 0; i < 8; i++)
+  {
+    sprintf(message, "Error in the index: %d", i);
+    
+    if (i < 2)
+    {
+      sort(a, length, (char *)type, i);
+    }
+    else if (i >= 2 && i < 5)
+    {
+      type = "On2";
+      sort(a, length, (char *)type, i);
+    }
+    else
+    {
+      type = "Onlogn";
+      sort(a, length, (char *)type, i);
+    }
+
+    TEST_ASSERT_EQUAL_STRING_ARRAY_MESSAGE(arrayOrdered, a, length, message);
+
+  }
+}
+
+TEST(Sort, TestNegativeValuesArray)
+{
+  int a[] = {-5, -4, -3, -2, -1};
+  int arrayOrdered[] = {-1, -2, -3, -4, -5};
+  int length = 5;
+  const char *type = "On";
+  char message[50];
+
+  for (int i = 0; i < 8; i++)
+  {
+    sprintf(message, "Error in the index: %d", i);
+
+    if (i < 2)
+    {
+      sort(a, length, (char *)type, i);
+    }
+    else if (i >= 2 && i < 5)
+    {
+      type = "On2";
+      sort(a, length, (char *)type, i);
+    }
+    else
+    {
+      type = "Onlogn";
+      sort(a, length, (char *)type, i);
+    }
+
+    TEST_ASSERT_EQUAL_INT32_ARRAY_MESSAGE(arrayOrdered, a, length, message);
+
+    for (int j = length; j > 0; j--)
+    {
+      a[length - j] = -j;
     }
   }
 }
--- Unity/T1/SortTester/test/test_runners/TestSort_Runner.c
+++ Unity/T1/SortTester/test/test_runners/TestSort_Runner.c
@@ -5,16 +5,23 @@ TEST_GROUP_RUNNER(Sort)
 {
   RUN_TEST_CASE(Sort, TestEverythingValid);
   RUN_TEST_CASE(Sort, TestInvalidType);
-  RUN_TEST_CASE(Sort, TestOutOfOrderArray);
+  RUN_TEST_CASE(Sort, TestInvalidIntType);
+  RUN_TEST_CASE(Sort, TestInvalidEmptyType);
+  RUN_TEST_CASE(Sort, TestValidButWrongType);
+  RUN_TEST_CASE(Sort, TestInvalidAlgorithm);
+  RUN_TEST_CASE(Sort, TestInvalidCharAlgorithm);
   RUN_TEST_CASE(Sort, TestWrongBiggerLenght);
-  //RUN_TEST_CASE(Sort, TestWrongSmallerLenght);
+  RUN_TEST_CASE(Sort, TestInvalidCharLenght);
+  RUN_TEST_CASE(Sort, TestOutOfOrderArray);
   RUN_TEST_CASE(Sort, TestTwoValuesArray);
   RUN_TEST_CASE(Sort, TestTwentyValuesArray);
   RUN_TEST_CASE(Sort, TestSameValuesArray);
-  RUN_TEST_CASE(Sort, TestValidButWrongType);
-  //RUN_TEST_CASE(Sort, TestEmptyArray);
   RUN_TEST_CASE(Sort, TestOneValueArray);
   RUN_TEST_CASE(Sort, TestOrderedArray);
   RUN_TEST_CASE(Sort, TestLowerHalfArray);
   RUN_TEST_CASE(Sort, TestUpperHalfArray);
+  //RUN_TEST_CASE(Sort, TestEmptyArray);
+  //RUN_TEST_CASE(Sort, TestFloatValueArray);
+  //RUN_TEST_CASE(Sort, TestCharValueArray);
+  //RUN_TEST_CASE(Sort, TestNegativeValuesArray);
 }
